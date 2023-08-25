6b7f4124897245003fc342383f8e72d16a4a0a7c
Victor Zhukov <v.zhukov@simplex48.ru>
2023-08-25 17:07:02 +0300

    +Новые решения

---
+++ LeetCodeSolutions/CanMakeArithmeticProgressionSolution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public sealed class CanMakeArithmeticProgressionSolution
+    {
+        public bool CanMakeArithmeticProgression(int[] arr)
+        {
+            int _StepProgression = int.MaxValue;
+            List<int> _StepsOfProgression = new List<int>();
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int _TempStep = Math.Abs(arr[i] - arr[i + 1]);
+
+                _StepsOfProgression.Add(_TempStep);
+
+                if (_TempStep < _StepProgression)
+                {
+                    _StepProgression = _TempStep;
+                }
+            }
+
+            if (_StepProgression == 1)
+            {
+                return !_StepsOfProgression.Any(step => step != 1);
+            }
+
+            return _StepsOfProgression.All(step => step % _StepProgression == 0);
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/CanMakeArithmeticProgressionSolutionTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCodeSolutions;
+using Xunit;
+
+namespace LeetCodeSolutionsTests
+{
+    public sealed class CanMakeArithmeticProgressionSolutionTests
+    {
+        [Fact]
+        public void FirstExample()
+        {
+            CanMakeArithmeticProgressionSolution sut = new CanMakeArithmeticProgressionSolution();
+            int[] _Array = new int[]
+            {
+                3, 5, 1
+            };
+
+            bool result = sut.CanMakeArithmeticProgression(_Array);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void SecondExample()
+        {
+            CanMakeArithmeticProgressionSolution sut = new CanMakeArithmeticProgressionSolution();
+            int[] _Array = new int[]
+            {
+                1, 2, 4
+            };
+
+            bool result = sut.CanMakeArithmeticProgression(_Array);
+
+            Assert.False(result);
+        }
+    }
+}
