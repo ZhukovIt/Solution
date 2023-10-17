dd3a6fcc508f8acb290cabd57d28f303d5a15a01
Victor Zhukov <v.zhukov@simplex48.ru>
2023-10-17 11:23:37 +0300

    +Решение

---
+++ LeetCodeSolutions/RichestCustomerWealthSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public sealed class RichestCustomerWealthSolution
+    {
+        public int MaximumWealth(int[][] accounts)
+        {
+            return accounts.Max(acc => acc.Sum());
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/RichestCustomerWealthSolutionTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCodeSolutions;
+using Xunit;
+
+namespace LeetCodeSolutionsTests
+{
+    public sealed class RichestCustomerWealthSolutionTests
+    {
+        [Fact]
+        public void FirstExample()
+        {
+            RichestCustomerWealthSolution sut = new RichestCustomerWealthSolution();
+            int[][] accounts = new int[][]
+            {
+                new int[] { 1, 2, 3 },
+                new int[] { 3, 2, 1 }
+            };
+
+            int result = sut.MaximumWealth(accounts);
+
+            Assert.Equal(6, result);
+        }
+        //----------------------------------------------------------------------------------
+        [Fact]
+        public void SecondExample()
+        {
+            RichestCustomerWealthSolution sut = new RichestCustomerWealthSolution();
+            int[][] accounts = new int[][]
+            {
+                new int[] { 1, 5 },
+                new int[] { 7, 3 },
+                new int[] { 3, 5 }
+            };
+
+            int result = sut.MaximumWealth(accounts);
+
+            Assert.Equal(10, result);
+        }
+        //----------------------------------------------------------------------------------
+        [Fact]
+        public void ThirdExample()
+        {
+            RichestCustomerWealthSolution sut = new RichestCustomerWealthSolution();
+            int[][] accounts = new int[][]
+            {
+                new int[] { 2, 8, 7 },
+                new int[] { 7, 1, 3 },
+                new int[] { 1, 9, 5 }
+            };
+
+            int result = sut.MaximumWealth(accounts);
+
+            Assert.Equal(17, result);
+        }
+        //----------------------------------------------------------------------------------
+    }
+}
